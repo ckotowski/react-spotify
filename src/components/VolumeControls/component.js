dcc9b86081dc0d5030ad71e1d772d2c490a0a61e
Chris Kotowski <ckotowski@gmail.com>
2020-05-25 00:52:18 -0400

    Upgraded to React 16.13 to get hooks support.  Converted VolumeControl to use hooks.

--- src/components/VolumeControls/component.js
+++ src/components/VolumeControls/component.js
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import "./VolumeControls.css";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './VolumeControls.css';
 
-class VolumeControls extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      volume: props.volume
-    };
-  }
+const VolumeControls = (props) => {
+  const [volume, setVolume] = useState(props.volume);
+  useEffect(() => {
+    console.log('volume', volume);
+  });
 
-  updateVolume = e => {
-    this.setState({
-      volume: e.target.value
-    });
-
-    this.props.updateVolume(Math.ceil(e.target.value / 10) * 10);
+  const updateVolume = (e) => {
+    setVolume(e.target.value);
+    props.updateVolume(Math.ceil(e.target.value / 10) * 10);
   };
 
-  render() {
-    return (
-      <div className="volume-container">
-        <i className="fa fa-volume-up" aria-hidden="true" />
-        <input
-          className="volume"
-          type="range"
-          min={0}
-          max={100}
-          value={this.state.volume}
-          onChange={this.updateVolume}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="volume-container">
+      <i className="fa fa-volume-up" aria-hidden="true" />
+      <input
+        className="volume"
+        type="range"
+        min={0}
+        max={100}
+        value={volume}
+        onChange={updateVolume}
+      />
+    </div>
+  );
+};
 
 VolumeControls.propTypes = {
   volume: PropTypes.number,
-  updateVolume: PropTypes.func
+  updateVolume: PropTypes.func,
 };
 
 export default VolumeControls;
