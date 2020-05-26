de57fec6eb2742c6feb2bcac1e72c42e40d3654a
Chris Kotowski <ckotowski@gmail.com>
2020-05-26 00:22:12 -0400

    Converted SideMenu to react-redux hooks

--- src/components/SideMenu/component.js
+++ src/components/SideMenu/component.js
@@ -1,57 +1,60 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./SideMenu.css";
-
-const SideMenu = ({
-  updateHeaderTitle,
-  updateViewType,
-  fetchFeatured,
-  fetchRecentlyPlayed,
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
   fetchSongs,
-  fetchAlbums,
-  fetchArtists,
-  token,
-  title,
-  artistIds
-}) => {
-  const handleClick = name => {
-    updateHeaderTitle(name);
-    updateViewType(name);
+  fetchRecentlyPlayed,
+  updateViewType,
+} from '../../actions/songActions';
+import { fetchAlbums } from '../../actions/albumActions';
+import { fetchArtists } from '../../actions/artistActions';
+import { fetchFeatured } from '../../actions/browseActions';
+import { updateHeaderTitle } from '../../actions/uiActions';
+import './SideMenu.css';
+
+const SideMenu = () => {
+  const token = useSelector((state) => state.tokenReducer.token);
+  const artistIds = useSelector((state) => state.artistsReducer.artistIds);
+  const title = useSelector((state) => state.uiReducer.title);
+  const dispatch = useDispatch();
+
+  const handleClick = (name) => {
+    dispatch(updateHeaderTitle(name));
+    dispatch(updateViewType(name));
   };
 
   const handleBrowseClick = () => {
-    updateHeaderTitle("Browse");
-    updateViewType("Featured");
-    fetchFeatured(token);
+    dispatch(updateHeaderTitle('Browse'));
+    dispatch(updateViewType('Featured'));
+    dispatch(fetchFeatured(token));
   };
 
   const renderSideMenu = () => {
     const menu = [
       {
-        name: "Recently Played",
-        action: fetchRecentlyPlayed
+        name: 'Recently Played',
+        action: fetchRecentlyPlayed,
       },
       {
-        name: "Songs",
-        action: fetchSongs
+        name: 'Songs',
+        action: fetchSongs,
       },
       {
-        name: "Albums",
-        action: fetchAlbums
+        name: 'Albums',
+        action: fetchAlbums,
       },
       {
-        name: "Artists",
+        name: 'Artists',
         action: fetchArtists,
-        getArtists: true
-      }
+        getArtists: true,
+      },
     ];
 
-    return menu.map(item => {
+    return menu.map((item) => {
       return (
         <li
           key={item.name}
           className={
-            title === item.name ? "active side-menu-item" : "side-menu-item"
+            title === item.name ? 'active side-menu-item' : 'side-menu-item'
           }
           onClick={() => {
             item.getArtists
@@ -71,7 +74,7 @@ const SideMenu = ({
       <li
         onClick={handleBrowseClick}
         className={
-          title === "Browse" ? "active side-menu-item" : "side-menu-item"
+          title === 'Browse' ? 'active side-menu-item' : 'side-menu-item'
         }
       >
         Browse
@@ -81,19 +84,6 @@ const SideMenu = ({
       {renderSideMenu()}
     </ul>
   );
-};
-
-SideMenu.propTypes = {
-  updateHeaderTitle: PropTypes.func,
-  updateViewType: PropTypes.func,
-  fetchFeatured: PropTypes.func,
-  fetchRecentlyPlayed: PropTypes.func,
-  fetchSongs: PropTypes.func,
-  fetchAlbums: PropTypes.func,
-  fetchArtists: PropTypes.func,
-  token: PropTypes.string,
-  artistIds: PropTypes.string,
-  title: PropTypes.string
 };
 
 export default SideMenu;
